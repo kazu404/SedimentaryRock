094defac66916e334f21a6238d0a429fa996f9bd
kazu404 <kazu@ris.ac.jp>
2025-05-26 16:25:43 +0900

    Add files via upload

---
+++ data/directories.js
@@ -0,0 +1,6 @@
+module.exports = {
+    css: "assets/css",
+    font: "assets/font",
+    img: "assets/img",
+    js: "assets/js"
+};
---
+++ data/site.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: "YOUR SITE NAME GOES HERE",
+    lang: "ja",
+    charset: "UTF-8",
+    dir: "ltr",
+    description: "YOUR SITE DESCRITION GOES HERE",
+    viewport: "width=device-width, initial-scale=1",
+    url: "http://YOUR-SITE.COM/",
+    titleSeparator: " - ",
+    og: {
+        title: "",
+        description: "",
+        type: "website",
+        url: "http://YOUR-SITE.COM/",
+        image: ""
+    },
+    social: {
+        facebook: "https://www.facebook.com/USERNAME",
+        googleplus: "https://plus.google.com/USERNAME",
+        instagram: "https://instagram.com/USERNAME",
+        twitter: "https://twitter.com/USERNAME"
+    },
+    ga: {
+        trackingId: "UA-XXXXX-X"
+    }
+};
